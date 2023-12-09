83bf9092cea638ff0cef738c3f7db9e77db79436
DucHuy2801 <theflash28012002@gmail.com>
2023-12-09 14:11:32 +0700

    fix forgotpassword

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -4,10 +4,11 @@ const express = require("express")
 const accessController = require("../../controllers/access.controller")
 const router = express.Router()
 const { asyncHandler } = require("../../helpers/asyncHandler")
+const { authenticationStudent } = require("../../auth/authUtils")
 
 router.post('/user/register', asyncHandler(accessController.register))
 router.post('/user/login', asyncHandler(accessController.login))
-
+router.use(authenticationStudent)
 router.post('/user/forgotpassword', asyncHandler(accessController.forgotPassword))
 router.post('/user/resetpassword', asyncHandler(accessController.resetPassword))
 
