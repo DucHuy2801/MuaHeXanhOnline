599f380219d23c682c5e18df191d4d0e395167d2
Quyen Nguyen <quyen.nguyentuan1112@gmail.com>
2023-12-10 19:17:02 +0700

    update authen

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -64,44 +64,44 @@ const authenticationAdmin = asyncHandler(async (req, res, next) => {
     }
 })
 
-// const authenticationLeader = asyncHandler(async (req, res, next) => {
-//     const token = req.headers[HEADER.AUTHORIZATION]
-//     if (!token) throw new AuthFailureError('Invalid Request')
-
-//     try {
-//         const decodeUser = JWT.decode(token)
-//         console.log(`decodeUser`, decodeUser)
-//         if (decodeUser.type_user !== 'leader') throw new AuthFailureError('Unauthorized')
-//         req.user = decodeUser
-//         return next()
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
-
 const authenticationLeader = asyncHandler(async (req, res, next) => {
-    const authHeader = req.headers[HEADER.AUTHORIZATION];
-    
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new AuthFailureError('Invalid Authorization Header');
-    }
+    const token = req.headers[HEADER.AUTHORIZATION]
+    if (!token) throw new AuthFailureError('Invalid Request')
 
-    const token = authHeader.slice(7); // Loại bỏ phần "Bearer "
-    console.log("token: ", token);
-    JWT.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            // JWT không hợp lệ
-            res.status(500).json({ error: err });
-            console.error('JWT verification failed:', err);
-        } else {
-            // JWT hợp lệ, decoded chứa thông tin từ payload
-            console.log('Decoded JWT:', decoded);
-            if (decoded.type_user !== 'leader') throw new AuthFailureError('Unauthorized')
-            req.user = decoded
-            return next()
-        }
-    });
+    try {
+        const decodeUser = JWT.decode(token)
+        console.log(`decodeUser`, decodeUser)
+        if (decodeUser.type_user !== 'leader') throw new AuthFailureError('Unauthorized')
+        req.user = decodeUser
+        return next()
+    } catch (error) {
+        console.log(error)
+    }
 })
+
+// const authenticationLeader = asyncHandler(async (req, res, next) => {
+//     const authHeader = req.headers[HEADER.AUTHORIZATION];
+    
+//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//         throw new AuthFailureError('Invalid Authorization Header');
+//     }
+
+//     const token = authHeader.slice(7); // Loại bỏ phần "Bearer "
+//     console.log("token: ", token);
+//     JWT.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//         if (err) {
+//             // JWT không hợp lệ
+//             res.status(500).json({ error: err });
+//             console.error('JWT verification failed:', err);
+//         } else {
+//             // JWT hợp lệ, decoded chứa thông tin từ payload
+//             console.log('Decoded JWT:', decoded);
+//             if (decoded.type_user !== 'leader') throw new AuthFailureError('Unauthorized')
+//             req.user = decoded
+//             return next()
+//         }
+//     });
+// })
 
 module.exports = {
     createTokenPair,
