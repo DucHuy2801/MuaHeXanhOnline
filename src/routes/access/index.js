5c6e990c02e7565b43dcef3db0a2f045f2538b84
DucHuy2801 <theflash28012002@gmail.com>
2023-12-10 14:17:31 +0700

    fix authen

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -8,8 +8,7 @@ const { authenticationStudent } = require("../../auth/authUtils")
 
 router.post('/user/register', asyncHandler(accessController.register))
 router.post('/user/login', asyncHandler(accessController.login))
-router.use(authenticationStudent)
-router.post('/user/forgotpassword', asyncHandler(accessController.forgotPassword))
-router.post('/user/resetpassword', asyncHandler(accessController.resetPassword))
+router.post('/user/forgotpassword', authenticationStudent, asyncHandler(accessController.forgotPassword))
+router.post('/user/resetpassword', authenticationStudent, asyncHandler(accessController.resetPassword))
 
 module.exports = router
